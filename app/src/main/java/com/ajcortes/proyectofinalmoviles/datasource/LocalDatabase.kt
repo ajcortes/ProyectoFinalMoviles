eac8c4ababee5396a944c49ac1ebd8fa7b746da3
ajcortes <ajcortes21@gmail.com>
2023-02-20 10:14:32 +0100

    Patata

--- app/src/main/java/com/ajcortes/proyectofinalmoviles/datasource/LocalDatabase.kt
+++ app/src/main/java/com/ajcortes/proyectofinalmoviles/datasource/LocalDatabase.kt
@@ -8,7 +8,7 @@ import com.ajcortes.proyectofinalmoviles.data.Movie
 
 @Database(
     entities = [Movie::class],
-    version = 2,
+    version = 1,
     exportSchema = false
 )
 abstract class LocalDatabase : RoomDatabase() {
